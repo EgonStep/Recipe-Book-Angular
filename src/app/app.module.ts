1b56485243d26debcc002d184c879b8fe0a187bf
EgonStep <041957Ab@>
2020-09-03 19:05:54 -0300

    add module per feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { MenubarModule } from 'primeng/menubar';
-import { DataViewModule } from 'primeng/dataview';
-import { DropdownModule } from 'primeng/dropdown';
-import { ListboxModule } from 'primeng/listbox';
-import { OrderListModule } from 'primeng/orderlist'
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
-import { DialogModule } from 'primeng/dialog';
 import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeService } from './recipes/recipe.service';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { DataStorageService } from './shared/services/data-storage.service';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthService } from './auth/auth.service';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AuthGuard } from './auth/auth.guard';
-import { AlertComponent } from './shared/modals/alert/alert.component';
-
-const primeNgImports = [
-  InputTextModule,
-  ButtonModule,
-  MenubarModule,
-  DataViewModule,
-  DropdownModule,
-  ListboxModule,
-  OrderListModule,
-  MessagesModule,
-  MessageModule,
-  DialogModule
-]
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -71,20 +25,10 @@ const primeNgImports = [
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    primeNgImports
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    RecipesResolverService,
-    DataStorageService,
-    AuthService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    },
-    AuthGuard
+    ShoppingListModule,
+    RecipesModule,
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
